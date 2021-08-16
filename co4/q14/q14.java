f8907b309826a1a59588f7a26a3e7c4fdf023cff
Aswin-1121 <75619250+Aswin-1121@users.noreply.github.com>
2021-08-16 10:51:32 +0530

    ** addition and deletion of elements in deque

---
+++ co4/q14/q14.java
@@ -0,0 +1,33 @@
+package c04;
+import java.util.*;
+import java.util.ArrayDeque;  
+import java.util.Deque;  
+  
+public class Q14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Deque<String> deque = new ArrayDeque<String>();   
+		 // Inserts the elements 
+	    Scanner B=new Scanner(System.in);
+		System.out.println("Enter the elements you want to insert:");
+		int n=B.nextInt();
+		System.out.println("Enter the elements : ");
+		for(int i =0;i<n;i++)
+		{
+			String obj=B.next();
+			deque.add(obj);
+	 
+		}
+	   
+	    // Popping the element  
+	    deque.pop();  
+	    System.out.println("After popping : ");  
+	    for (String string : deque) {  
+	        System.out.println(string);  
+	    }  
+	    deque.remove(2);  
+	    System.out.println("Queue becomes"+deque);  
+	}
+
+}
